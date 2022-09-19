cb2d669d50e5cdd7d4e66b3f8ccd65a93a20810f
Hideto Inoue <111981295+hidemaru-jp@users.noreply.github.com>
2022-09-19 10:53:06 +0000

    [FINISH]5-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,50 +1,66 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @books = Book.all
+    @user = User.find(current_user.id)
     if @book.save
-      flash[:success] = "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
+    else
+      render :index
     end
+    
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    @user = User.find(current_user.id)
 
   end
 
   def show
-    @book = Book.find(params[:id])
-    
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = User.find(@books.user_id)
+
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:success] = "You have updated book successfully."
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
     end
   end
-    
-  
+
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
 
-  
+
   private
 
   def book_params
-    params.require(:book).permit(:image, :title, :opinion)
+    params.require(:book).permit(:image, :title, :body)
   end
   
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: "homes#top"
+
+  root to: "home#top"
   devise_for :users
-  get "homes/about" => "homes#about", as: "about"
-  
+  get "home/about" => "home#about", as: "about"
+
   resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
-  
+  resources :users, only: [:index, :show, :edit, :update, :create]
+
 end
